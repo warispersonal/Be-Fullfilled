6de9bd271409f284025df56a0ee22f18e7fcc6b0
mwariszargar <muhammad.waris@ecologix.us>
2021-04-09 11:09:30 +0500

    Fix podcasts create and list

--- resources/views/admin/podcast/index.blade.php
+++ resources/views/admin/podcast/index.blade.php
@@ -18,8 +18,17 @@
             @foreach($podcasts as  $podcast)
                 <div class="{{$loop->index % 2 == 0 ?  "FlipThumb-Left" : "FlipThumb-Right"}}">
                     <div class="FlipSwitch-Thum">
-                        <img src="{{$podcast->image}}" class="flip-the-switch-image" alt=""/>
-
+                        @if($podcast->mediaType != 1)
+                            <img src="{{$podcast->image}}" class="flip-the-switch-image" alt=""/>
+                        @else
+                            <div class="VideoCon">
+                                <div class="holder-prod">
+                                    <video width="343" height="156" controls>
+                                        <source src="{{$podcast->media}}">
+                                    </video>
+                                </div>
+                            </div>
+                        @endif
                         <div class="FlipInner-Head">
                             <h2>{{$podcast->customizeDates}}</h2>
                             <h3>{{$podcast->title}}</h3>
@@ -27,8 +36,9 @@
 
                         <div class="Auto-Container">
                             <audio controls>
-                                <source src="horse.ogg" type="audio/ogg">
-                                <source src="{{$podcast->media}}" type="audio/mpeg">
+                                @if($podcast->mediaType != 1)
+                                    <source src="{{$podcast->media}}" type="audio/mpeg">
+                                @endif
                             </audio>
                         </div>
 
