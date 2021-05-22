a4996897999fbe70da37fed6eded14ba0b80d221
mwariszargar <muhammad.waris@ecologix.us>
2021-05-22 11:17:28 +0500

    Update validation for send reset passowrd on sms

--- app/Http/Controllers/api/v1/Twilio.php
+++ app/Http/Controllers/api/v1/Twilio.php
@@ -40,25 +40,30 @@ class Twilio extends Controller
 
     public function sendResetPasswordOnMobile(Request $request)
     {
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             'phone_number' => 'required'
         ]);
-        if (RecordExistsController::isCellNoExists($request->phone_number)) {
-            $account_sid = getenv("TWILIO_SID");
-            $auth_token = getenv("TWILIO_AUTH_TOKEN");
-            $twilio_number = getenv("TWILIO_NUMBER");
-            $client = new Client($account_sid, $auth_token);
-            $six_digit_random_number = mt_rand(1000, 9999);
-            $message = "Your reset password code is " . $six_digit_random_number;
-            $client->messages->create($request->phone_number, ['from' => $twilio_number, 'body' => $message]);
-            return $this->success('Code send successfully', ['code' => $six_digit_random_number]);
-            try {
-
-            } catch (TwilioException  $e) {
-                return $this->failure('This is not valid phone number');
-            }
+        if ($validator->fails()) {
+            $error = $validator->errors()->first();
+            return $this->validationFailure($error);
         } else {
-            return $this->failure('Sorry, This user is not exist in our system', 404);
+            if (RecordExistsController::isCellNoExists($request->phone_number)) {
+                $account_sid = getenv("TWILIO_SID");
+                $auth_token = getenv("TWILIO_AUTH_TOKEN");
+                $twilio_number = getenv("TWILIO_NUMBER");
+                $client = new Client($account_sid, $auth_token);
+                $six_digit_random_number = mt_rand(1000, 9999);
+                $message = "Your reset password code is " . $six_digit_random_number;
+                $client->messages->create($request->phone_number, ['from' => $twilio_number, 'body' => $message]);
+                return $this->success('Code send successfully', ['code' => $six_digit_random_number]);
+                try {
+
+                } catch (TwilioException  $e) {
+                    return $this->failure('This is not valid phone number');
+                }
+            } else {
+                return $this->failure('Sorry, This user is not exist in our system', 404);
+            }
         }
     }
 
