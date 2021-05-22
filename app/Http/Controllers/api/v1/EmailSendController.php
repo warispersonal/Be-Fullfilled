2d9cda8cc61f64ebf5a5f0579770ed23fcf26897
mwariszargar <muhammad.waris@ecologix.us>
2021-05-22 11:13:57 +0500

    Update validation for send reset passowrd on email

--- app/Http/Controllers/api/v1/EmailSendController.php
+++ app/Http/Controllers/api/v1/EmailSendController.php
@@ -6,20 +6,26 @@ use App\Http\Controllers\Controller;
 use App\Mail\ResetPasswordCode;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 
 class EmailSendController extends Controller
 {
     public function sendResetPasswordOnEmail(Request $request)
     {
-        $request->validate([
+        $validator = Validator::make($request->all(), [
             'email' => 'required'
         ]);
-        if (RecordExistsController::isEmailExists($request->email)) {
-            $six_digit_random_number = mt_rand(1000, 9999);
-            Mail::to($request->email)->send(new ResetPasswordCode($six_digit_random_number));
-            return $this->success('Code send successfully', ['code' => $six_digit_random_number]);
+        if ($validator->fails()) {
+            $error = $validator->errors()->first();
+            return $this->validationFailure($error);
         } else {
-            return $this->failure('Sorry, This user is not exist in our system', 404);
+            if (RecordExistsController::isEmailExists($request->email)) {
+                $six_digit_random_number = mt_rand(1000, 9999);
+                Mail::to($request->email)->send(new ResetPasswordCode($six_digit_random_number));
+                return $this->success('Code send successfully', ['code' => $six_digit_random_number]);
+            } else {
+                return $this->failure('Sorry, This user is not exist in our system', 404);
+            }
         }
     }
 }
