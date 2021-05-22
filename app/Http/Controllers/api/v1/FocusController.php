9374861fb47966158e8acf774d01c8799f4a6f99
mwariszargar <muhammad.waris@ecologix.us>
2021-05-22 11:47:39 +0500

    Update validation response for focus day create api

--- app/Http/Controllers/api/v1/FocusController.php
+++ app/Http/Controllers/api/v1/FocusController.php
@@ -8,24 +8,33 @@ use App\Http\Resources\FocusDayResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Date;
+use Illuminate\Support\Facades\Validator;
 
 class FocusController extends Controller
 {
 
     public function dayFocusScore(Request $request)
     {
-        $focus = FocusDay::where('user_id', Auth::id())->where('date', Date::now()->format('Y-m-d'))->first();
-        if ($focus) {
-            $focus->focus_value = $request->focus_value;
-            $focus->save();
-            return $this->success("Day Focus Updates", new FocusDayResource($focus));
+        $validator = Validator::make($request->all(), [
+            'focus_value' => 'required|numeric',
+        ]);
+        if ($validator->fails()) {
+            $error = $validator->errors()->first();
+            return $this->validationFailure($error);
         } else {
-            $focusDay = new FocusDay();
-            $focusDay->user_id = Auth::id();
-            $focusDay->date = Date::now()->format('Y-m-d');
-            $focusDay->focus_value = $request->focus_value;
-            $focusDay->save();
-            return $this->success("Day Focus Created", new FocusDayResource($focusDay));
+            $focus = FocusDay::where('user_id', Auth::id())->where('date', Date::now()->format('Y-m-d'))->first();
+            if ($focus) {
+                $focus->focus_value = $request->focus_value;
+                $focus->save();
+                return $this->success("Day Focus Updates", new FocusDayResource($focus));
+            } else {
+                $focusDay = new FocusDay();
+                $focusDay->user_id = Auth::id();
+                $focusDay->date = Date::now()->format('Y-m-d');
+                $focusDay->focus_value = $request->focus_value;
+                $focusDay->save();
+                return $this->success("Day Focus Created", new FocusDayResource($focusDay));
+            }
         }
 
     }
@@ -33,7 +42,7 @@ class FocusController extends Controller
     public function getDayFocusScore($date = null)
     {
         if ($date == null) {
-            $focus = FocusDay::where('date', Date::now()->format('Y-m-d'))->where('user_id',Auth::id())->first();
+            $focus = FocusDay::where('date', Date::now()->format('Y-m-d'))->where('user_id', Auth::id())->first();
         } else {
             $focus = FocusDay::where('date', $date)->first();
         }
