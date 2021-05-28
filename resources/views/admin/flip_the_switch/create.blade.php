749260bb127a52e5c164f37eb05be3907da6f528
mwariszargar <muhammad.waris@ecologix.us>
2021-05-28 13:47:42 +0500

    Single file upload with name show & add radio button

--- resources/views/admin/flip_the_switch/create.blade.php
+++ resources/views/admin/flip_the_switch/create.blade.php
@@ -1,5 +1,4 @@
 @extends('layouts.admin')
-
 @section('content')
     <div class="MainFlip-Container">
         <div class="FlipSwitch-Con">
@@ -11,54 +10,56 @@
             <div class="UploadNew-Container">
                 <div class="UploadTab">
                     <ul class="nav nav-tabs" id="myTab" role="tablist">
+
                         <li class="nav-item">
-                            <a class="nav-link active" id="home-tab" data-toggle="tab" href="#home" role="tab"
-                               aria-controls="home" aria-selected="true">Audio</a>
+                            <label class="radio-container-btn-power">Audio
+                                <input type="radio" name="radio">
+                                <span class="radiomark"></span>
+                            </label>
                         </li>
+
                         <li class="nav-item">
-                            <a class="nav-link" id="profile-tab" data-toggle="tab" href="#profile" role="tab"
-                               aria-controls="profile" aria-selected="false">Video</a>
+                            <label class="radio-container-btn-power">Video
+                                <input type="radio" name="radio">
+                                <span class="radiomark"></span>
+                            </label>
                         </li>
+
                     </ul>
                     <div class="tab-content" id="myTabContent">
                         <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
                             <div class="datainfo">
                                 <label>Select Date</label>
-                                <input type="date" id="" name="date" value="{{old('date')}}" placeholder="December 2, 2018">
+                                <input type="date" id="" name="date" value="{{old('date')}}"
+                                       placeholder="December 2, 2018">
                                 <i class="icon-calendar"></i>
                             </div>
                             <div class="datainfo">
                                 <label>Podcast Title</label>
                                 <input type="text" id="" name="title" value="{{old('title')}}" placeholder="">
                             </div>
-                            <div class="UploadContainer">
-                                <label>Upload Thumbnail Picture</label>
-                                <div class="upload-btn-wrapper">
-                                    <button class="btn"><i class="icon-upload"></i> Upload a file</button>
-                                    <input type="file" name="file"/>
-                                </div>
+                            <div class="form-group mt-4">
+                                <input type="file" name="file" id="file" class="input-file">
+                                <label for="file" class="btn btn-tertiary js-labelFile">
+                                    <i class="icon-upload"></i>
+                                    <span class="js-fileName">Upload a file</span>
+                                </label>
                             </div>
                             <div class="SpriteLine"></div>
-                            <div class="UploadContainer">
-                                <div class="upload-btn-wrapper">
-                                    <button class="btn"><i class="icon-upload"></i> Upload a file</button>
-                                    <input type="file" name="link"/>
-                                </div>
+                            <div class="form-group mt-4">
+                                <input type="file" name="link" id="file1" class="input-file">
+                                <label for="file" class="btn btn-tertiary js-labelFile">
+                                    <i class="icon-upload"></i>
+                                    <span class="js-fileName">Upload a file</span>
+                                </label>
                             </div>
-
                             <div class="SaveBtn">
                                 <button type="submit">Save File</button>
                             </div>
-
                         </div>
-
                         <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab"></div>
-
                     </div>
-
                 </div>
-
-
             </div>
         </form>
     </div>
--- resources/views/partials/admin/_footer.blade.php
+++ resources/views/partials/admin/_footer.blade.php
@@ -1,13 +1,28 @@
 <script src="{{asset('assets/js/app-dist.js')}}"></script>
 <script src="https://malihu.github.io/custom-scrollbar/jquery.mCustomScrollbar.concat.min.js"></script>
 <script>
-
     (function($){
         $(window).on("load",function(){
             $(".content").mCustomScrollbar();
         });
     })(jQuery);
+</script>
+<script>
+    (function() {
+        'use strict';
+        $('.input-file').each(function() {
+            var $input = $(this),
+                $label = $input.next('.js-labelFile'),
+                labelVal = $label.html();
 
+            $input.on('change', function(element) {
+                debugger
+                var fileName = '';
+                if (element.target.value) fileName = element.target.value.split('\\').pop();
+                fileName ? $label.addClass('has-file').find('.js-fileName').html(fileName) : $label.removeClass('has-file').html(labelVal);
+            });
+        });
+    })();
 </script>
 @yield('js')
 
