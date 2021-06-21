25ad4a724cbb64e5340bd97c64267e22a1d1c14b
mwariszargar <muhammad.waris@ecologix.us>
2021-06-21 17:59:14 +0500

    Create constant files for twillio & File

---
+++ app/Constant/FileConstant.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Constant;
+
+class FileConstant
+{
+    const FLIP_THE_SWITCH_IMAGES = 'images/flip_the_switch';
+    const FLIP_THE_SWITCH_MEDIA = 'media/flip_the_switch';
+
+    const CONTENT_LIBRARY_IMAGES = 'images/content_library';
+    const CONTENT_LIBRARY_MEDIA = 'media/content_library';
+
+    const  PODCASTS_IMAGES = 'images/podcasts';
+    const  PODCASTS_MEDIA = 'media/podcasts';
+
+    const  PRODUCTS_IMAGES = 'images/products';
+
+    const  PUSH_NOTIFICATION_IMAGES = 'images/notification';
+
+    const  ADMIN_IMAGES = 'images/admin';
+
+    const  USER_IMAGES = 'images/user';
+
+    const  BUG_REPORT_MEDIA = 'images/bug_report';
+}
---
+++ app/Constant/TwilioConstant.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Constant;
+
+class TwilioConstant
+{
+    const TWILIO_SID = "ACe1a8d38c382c1d74697166d2d9708ae1";
+    const TWILIO_AUTH_TOKEN = "125a364a1a344de6763119d003e12258";
+    const TWILIO_VERIFY_SID = "VA179988c737cf2047f6d6896011f7b4dd";
+    const TWILIO_NUMBER = "+15102964187";
+    const TWILIO_VERIFY_NUMBER = "+92 323 4812995";
+    const TWILIO_ACCOUNT_STATUS = false;
+}
