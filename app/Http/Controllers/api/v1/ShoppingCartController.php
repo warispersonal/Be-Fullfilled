8a3fad09d9b75c6468d08d60a039bb9a3b77a96b
mwariszargar <muhammad.waris@ecologix.us>
2021-06-22 17:31:19 +0500

    Shopping cart list

--- app/Http/Controllers/api/v1/ShoppingCartController.php
+++ app/Http/Controllers/api/v1/ShoppingCartController.php
@@ -5,15 +5,21 @@ namespace App\Http\Controllers\api\v1;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\ShoppingResource;
 use App\ShoppingCart;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Auth;
 
 class ShoppingCartController extends Controller
 {
-    public function index(){
-
+    public function index()
+    {
+        $carts = ShoppingCart::where('user_id', Auth::id())->get();
+        return $this->success('Shopping Cart List', ShoppingResource::collection($carts));
     }
-    public function store(Request $request){
+
+    public function store(Request $request)
+    {
         $shopping_cart = new ShoppingCart();
         $shopping_cart->product_id = $request->product_id;
         $shopping_cart->user_id = Auth::id();
@@ -21,7 +27,9 @@ class ShoppingCartController extends Controller
         $shopping_cart->save();
         return $this->success('Shopping Cart Added', new ShoppingResource($shopping_cart));
     }
-    public function destroy($id){
+
+    public function destroy($id)
+    {
 
     }
 }
