c7d00bd6cd98783b2f3be221acad4ddf20c832da
mwariszargar <muhammad.waris@ecologix.us>
2021-07-01 11:11:44 +0500

    Shopping cart api updated

--- app/Http/Controllers/api/v1/ShoppingCartController.php
+++ app/Http/Controllers/api/v1/ShoppingCartController.php
@@ -8,7 +8,9 @@ use App\Http\Resources\ShoppingCollection;
 use App\Http\Resources\ShoppingResource;
 use App\Notes;
 use App\ShoppingCart;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 use Illuminate\Support\Facades\Auth;
 
@@ -23,12 +25,20 @@ class ShoppingCartController extends Controller
 
     public function store(Request $request)
     {
-        $shopping_cart = new ShoppingCart();
-        $shopping_cart->product_id = $request->product_id;
-        $shopping_cart->user_id = Auth::id();
-        $shopping_cart->quantity = $request->quantity;
-        $shopping_cart->save();
-        return $this->success('Shopping Cart Added', new ShoppingResource($shopping_cart));
+        $shopping_cart = ShoppingCart::where("product_id", $request->product_id)->where("user_id", Auth::id())->get();
+        if (count($shopping_cart) > 0) {
+            $shopping_cart = $shopping_cart[0];
+            $shopping_cart->quantity = ((int)$request->quantity + (int)$shopping_cart->quantity);
+            $shopping_cart->save();
+            return $this->success('Shopping Cart Added', new ShoppingResource($shopping_cart));
+        } else {
+            $shopping_cart = new ShoppingCart();
+            $shopping_cart->product_id = $request->product_id;
+            $shopping_cart->user_id = Auth::id();
+            $shopping_cart->quantity = $request->quantity;
+            $shopping_cart->save();
+            return $this->success('Shopping Cart Added', new ShoppingResource($shopping_cart));
+        }
     }
 
     public function destroy($id)
