e1bef7ef9f16f82e042b44dc5583cfd87a3f2d98
mwariszargar <muhammad.waris@ecologix.us>
2021-07-06 10:56:23 +0500

    Feedback validation

--- app/Http/Controllers/api/v1/FeedBackController.php
+++ app/Http/Controllers/api/v1/FeedBackController.php
@@ -7,16 +7,26 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\FeedBackResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class FeedBackController extends Controller
 {
     public function store(Request $request)
     {
-        $feedback = new FeedBack();
-        $feedback->service_status = $request->service_status;
-        $feedback->what_wrong = $request->what_wrong;
-        $feedback->user_id = Auth::id();
-        $feedback->save();
-        return $this->success("Thanks for sharing feedback", new FeedBackResource($feedback));
+        $validator = Validator::make($request->all(), [
+            'service_status' => 'required',
+            'what_wrong' => 'required',
+        ]);
+        if ($validator->fails()) {
+            $error = $validator->errors()->first();
+            return $this->validationFailure($error);
+        } else {
+            $feedback = new FeedBack();
+            $feedback->service_status = $request->service_status;
+            $feedback->what_wrong = $request->what_wrong;
+            $feedback->user_id = Auth::id();
+            $feedback->save();
+            return $this->success("Thanks for sharing feedback", new FeedBackResource($feedback));
+        }
     }
 }
