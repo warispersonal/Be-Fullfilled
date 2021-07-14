47fe4ca773d41f7d160be1ed417b74d9837ef497
mwariszargar <muhammad.waris@ecologix.us>
2021-07-14 11:45:32 +0500

    Cart api flow update

--- app/Http/Controllers/api/v1/ShoppingCartController.php
+++ app/Http/Controllers/api/v1/ShoppingCartController.php
@@ -27,18 +27,45 @@ class ShoppingCartController extends Controller
     public function store(Request $request)
     {
         $shopping_cart = ShoppingCart::where("product_id", $request->product_id)->where("user_id", Auth::id())->get();
-        if (count($shopping_cart) > 0) {
-            $shopping_cart = $shopping_cart[0];
-            $shopping_cart->quantity = ((int)$request->quantity + (int)$shopping_cart->quantity);
-            $shopping_cart->save();
-            return $this->success('Shopping Cart Added', new ShoppingResource($shopping_cart));
-        } else {
-            $shopping_cart = new ShoppingCart();
-            $shopping_cart->product_id = $request->product_id;
-            $shopping_cart->user_id = Auth::id();
-            $shopping_cart->quantity = $request->quantity;
-            $shopping_cart->save();
-            return $this->success('Shopping Cart Added', new ShoppingResource($shopping_cart));
+        if($request->cart_status == "add_to_cart")
+        {
+            if($request->quantity > 0 ){
+                if (count($shopping_cart) > 0) {
+                    $shopping_cart = $shopping_cart[0];
+                    $shopping_cart->quantity = ((int)$request->quantity + (int)$shopping_cart->quantity);
+                    $shopping_cart->save();
+                    return $this->success('Shopping Cart Added', new ShoppingResource($shopping_cart));
+                }
+                else {
+                    $shopping_cart = new ShoppingCart();
+                    $shopping_cart->product_id = $request->product_id;
+                    $shopping_cart->user_id = Auth::id();
+                    $shopping_cart->quantity = $request->quantity;
+                    $shopping_cart->save();
+                    return $this->success('Shopping Cart Added', new ShoppingResource($shopping_cart));
+                }
+            }else{
+                return $this->failure('Product not added to cart because quantity is 0');
+            }
+        }
+        elseif ($request->cart_status == "quantity_update"){
+            if($request->quantity > 0 ) {
+                if (count($shopping_cart) > 0) {
+                    $shopping_cart = $shopping_cart[0];
+                    $shopping_cart->quantity = $request->quantity;
+                    $shopping_cart->save();
+                    return $this->success('Quantity updated', new ShoppingResource($shopping_cart));
+                }
+            }
+            else{
+                foreach ($shopping_cart as $cart){
+                    $cart->delete();
+                }
+                return $this->success("Remove product from shopping cart");
+            }
+        }
+        else{
+            return $this->failure('Flag is not set');
         }
     }
 
@@ -47,9 +74,9 @@ class ShoppingCartController extends Controller
         $shopping_cart = ShoppingCart::find($id);
         if ($shopping_cart) {
             $shopping_cart->delete();
-            return $this->success("Shopping Cart Deleted");
+            return $this->success("Remove product from shopping cart");
         } else {
-            return $this->failure('Shopping Cart not found', 404);
+            return $this->failure('Product not found in cart', 404);
         }
     }
 }
