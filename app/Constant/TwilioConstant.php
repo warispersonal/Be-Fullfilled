0a45b0ec545a492b87bbc52791533096cab62ef7
warispersonal <83630258+warispersonal@users.noreply.github.com>
2021-08-09 13:15:09 +0500

    Update TwilioConstant.php

--- app/Constant/TwilioConstant.php
+++ app/Constant/TwilioConstant.php
@@ -4,9 +4,9 @@ namespace App\Constant;
 
 class TwilioConstant
 {
-    const TWILIO_SID = "ACe1a8d38c382c1d74697166d2d9708ae1";
-    const TWILIO_AUTH_TOKEN = "125a364a1a344de6763119d003e12258";
-    const TWILIO_VERIFY_SID = "VA179988c737cf2047f6d6896011f7b4dd";
-    const TWILIO_NUMBER = "+15102964187";
+    const TWILIO_SID = "";
+    const TWILIO_AUTH_TOKEN = "";
+    const TWILIO_VERIFY_SID = "";
+    const TWILIO_NUMBER = "";
 
 }
